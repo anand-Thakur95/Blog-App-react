cd605f48c48a2e387fcc52fe4f699e86f95304e9
Anand Thakur <satul95060@gmail.com>
2025-07-05 23:57:57 +0530

    fix Editor

--- frontend/src/components/Editor.jsx
+++ frontend/src/components/Editor.jsx
@@ -1,221 +1,133 @@
-import { useState, useEffect, useRef, useMemo } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-import 'ckeditor5/ckeditor5.css';
-
 // import './App.css';
 
-/**
- * Create a free account with a trial: https://portal.ckeditor.com/checkout?plan=free
- */
-const LICENSE_KEY = 'GPL'; // or <YOUR_LICENSE_KEY>.
-
 export default function Editor({ props }) {
-	const editorContainerRef = useRef(null);
-	const editorRef = useRef(null);
-	const editorWordCountRef = useRef(null);
-	const [isLayoutReady, setIsLayoutReady] = useState(false);
+    const editorContainerRef = useRef(null);
+    const editorRef = useRef(null);
+    const editorWordCountRef = useRef(null);
+    const [isLayoutReady, setIsLayoutReady] = useState(false);
 
-	useEffect(() => {
-		setIsLayoutReady(true);
+    useEffect(() => {
+        setIsLayoutReady(true);
+        return () => setIsLayoutReady(false);
+    }, []);
 
-		return () => setIsLayoutReady(false);
-	}, []);
+    const editorConfig = {
+        toolbar: {
+            items: [
+                'heading',
+                '|',
+                'bold',
+                'italic',
+                'underline',
+                '|',
+                'link',
+                'insertImage',
+                'mediaEmbed',
+                'insertTable',
+                'blockQuote',
+                '|',
+                'bulletedList',
+                'numberedList',
+                '|',
+                'outdent',
+                'indent'
+            ],
+            shouldNotGroupWhenFull: false
+        },
+        heading: {
+            options: [
+                {
+                    model: 'paragraph',
+                    title: 'Paragraph',
+                    class: 'ck-heading_paragraph'
+                },
+                {
+                    model: 'heading1',
+                    view: 'h1',
+                    title: 'Heading 1',
+                    class: 'ck-heading_heading1'
+                },
+                {
+                    model: 'heading2',
+                    view: 'h2',
+                    title: 'Heading 2',
+                    class: 'ck-heading_heading2'
+                },
+                {
+                    model: 'heading3',
+                    view: 'h3',
+                    title: 'Heading 3',
+                    class: 'ck-heading_heading3'
+                },
+                {
+                    model: 'heading4',
+                    view: 'h4',
+                    title: 'Heading 4',
+                    class: 'ck-heading_heading4'
+                },
+                {
+                    model: 'heading5',
+                    view: 'h5',
+                    title: 'Heading 5',
+                    class: 'ck-heading_heading5'
+                },
+                {
+                    model: 'heading6',
+                    view: 'h6',
+                    title: 'Heading 6',
+                    class: 'ck-heading_heading6'
+                }
+            ]
+        },
+        image: {
+            toolbar: [
+                'toggleImageCaption',
+                'imageTextAlternative',
+                '|',
+                'imageStyle:inline',
+                'imageStyle:wrapText',
+                'imageStyle:breakText',
+                '|',
+                'resizeImage'
+            ]
+        },
+        link: {
+            addTargetToExternalLinks: true,
+            defaultProtocol: 'https://'
+        },
+        table: {
+            contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+        },
+        placeholder: 'Type or paste your content here!'
+    };
 
-	const { editorConfig } = useMemo(() => {
-		if (!isLayoutReady) {
-			return {};
-		}
-
-		return {
-			editorConfig: {
-				toolbar: {
-					items: [
-						'sourceEditing',
-						'showBlocks',
-						'textPartLanguage',
-						'|',
-						'heading',
-						'|',
-						'bold',
-						'italic',
-						'underline',
-						'|',
-						'emoji',
-						'pageBreak',
-						'link',
-						'insertImage',
-						'insertTable',
-						'blockQuote',
-						'htmlEmbed',
-						'|',
-						'outdent',
-						'indent'
-					],
-					shouldNotGroupWhenFull: false
-				},
-				plugins: [
-					Autoformat,
-					AutoImage,
-					Autosave,
-					BalloonToolbar,
-					Base64UploadAdapter,
-					BlockQuote,
-					BlockToolbar,
-					Bold,
-					Emoji,
-					Essentials,
-					FullPage,
-					GeneralHtmlSupport,
-					Heading,
-					HtmlComment,
-					HtmlEmbed,
-					ImageBlock,
-					ImageInsert,
-					ImageInsertViaUrl,
-					ImageToolbar,
-					ImageUpload,
-					Indent,
-					IndentBlock,
-					Italic,
-					Link,
-					Mention,
-					PageBreak,
-					Paragraph,
-					ShowBlocks,
-					SourceEditing,
-					Table,
-					TableCaption,
-					TableCellProperties,
-					TableColumnResize,
-					TableProperties,
-					TableToolbar,
-					TextPartLanguage,
-					TextTransformation,
-					Title,
-					Underline,
-					WordCount
-				],
-				balloonToolbar: ['bold', 'italic', '|', 'link', 'insertImage'],
-				blockToolbar: ['bold', 'italic', '|', 'link', 'insertImage', 'insertTable', '|', 'outdent', 'indent'],
-				heading: {
-					options: [
-						{
-							model: 'paragraph',
-							title: 'Paragraph',
-							class: 'ck-heading_paragraph'
-						},
-						{
-							model: 'heading1',
-							view: 'h1',
-							title: 'Heading 1',
-							class: 'ck-heading_heading1'
-						},
-						{
-							model: 'heading2',
-							view: 'h2',
-							title: 'Heading 2',
-							class: 'ck-heading_heading2'
-						},
-						{
-							model: 'heading3',
-							view: 'h3',
-							title: 'Heading 3',
-							class: 'ck-heading_heading3'
-						},
-						{
-							model: 'heading4',
-							view: 'h4',
-							title: 'Heading 4',
-							class: 'ck-heading_heading4'
-						},
-						{
-							model: 'heading5',
-							view: 'h5',
-							title: 'Heading 5',
-							class: 'ck-heading_heading5'
-						},
-						{
-							model: 'heading6',
-							view: 'h6',
-							title: 'Heading 6',
-							class: 'ck-heading_heading6'
-						}
-					]
-				},
-				htmlSupport: {
-					allow: [
-						{
-							name: /^.*$/,
-							styles: true,
-							attributes: true,
-							classes: true
-						}
-					]
-				},
-				image: {
-					toolbar: ['imageTextAlternative']
-				},
-				initialData: props?.initialData || '',
-				licenseKey: LICENSE_KEY,
-				link: {
-					addTargetToExternalLinks: true,
-					defaultProtocol: 'https://',
-					decorators: {
-						toggleDownloadable: {
-							mode: 'manual',
-							label: 'Downloadable',
-							attributes: {
-								download: 'file'
-							}
-						}
-					}
-				},
-				mention: {
-					feeds: [
-						{
-							marker: '@',
-							feed: [
-								/* See: https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html */
-							]
-						}
-					]
-				},
-				placeholder: 'Type or paste your content here!',
-				table: {
-					contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
-				}
-			}
-		};
-	}, [isLayoutReady]);
-
-	return (
-		<div className="main-container">
-			<div
-				className="editor-container editor-container_classic-editor editor-container_include-block-toolbar editor-container_include-word-count"
-				ref={editorContainerRef}
-			>
-				<div className="editor-container__editor">
-					<div ref={editorRef}>
-						{editorConfig && (
-							<CKEditor
-								onChange={props.onChange}
-								onReady={editor => {
-									const wordCount = editor.plugins.get('WordCount');
-									editorWordCountRef.current.appendChild(wordCount.wordCountContainer);
-								}}
-								onAfterDestroy={() => {
-									Array.from(editorWordCountRef.current.children).forEach(child => child.remove());
-								}}
-								editor={ClassicEditor}
-								config={editorConfig}
-							/>
-						)}
-					</div>
-				</div>
-				<div className="editor_container__word-count" ref={editorWordCountRef}></div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="main-container">
+            <div
+                className="editor-container editor-container_classic-editor"
+                ref={editorContainerRef}
+            >
+                <div className="editor-container__editor">
+                    <div ref={editorRef}>
+                        {isLayoutReady && (
+                            <CKEditor
+                                editor={ClassicEditor}
+                                config={editorConfig}
+                                data={props?.initialData || ''}
+                                onChange={props.onChange}
+                                onReady={editor => {
+                                    // Editor is ready to use
+                                  
+                                }}
+                            />
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
